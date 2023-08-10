bbc2b8b1a515f93dfaa59ebc9fce661b311ad32a
MuniozSanti <muniozsanti@gmail.com>
2023-08-10 18:43:53 -0300

    Pre-entrega1 finalizada

---
+++ Pre-entrega1-Munoz/src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from './components/Navbar/navbar'
+import ItemListContainer from './components/ItemListContainer/Itemlistcontainer'
+
+function App() {
+  return (
+    <div>
+ 
+      <NavBar />
+      
+      <ItemListContainer greeting="Hola Mundo!" />
+    </div>
+  )
+}
+
+export default App
+  
